6bee20a65d60875ff6042e2d15e79ee9cceec2e8
taylorod <taylorsunwantedemails@gmail.com>
2020-10-22 20:31:10 -0500

    all crud features for reachouts

--- reachout_backend/app/controllers/api/contacts_controller.rb
+++ reachout_backend/app/controllers/api/contacts_controller.rb
@@ -9,7 +9,6 @@ class Api::ContactsController < ApplicationController
       first_name: params[:first_name],
       last_name: params[:last_name],
       phone_number: params[:phone_number],
-      # user_id: params[:user_id],
     )
     @contact.save
     render "show.json.jb"
@@ -21,6 +20,12 @@ class Api::ContactsController < ApplicationController
   end
 
   def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.name = params[:name] || @contact.name
+    @contact.width = params[:width] || @contact.width
+    @contact.height = params[:height] || @contact.height
+    @contact.save
+    render "show.json.jb"
   end
 
   def destroy
--- reachout_backend/app/controllers/api/reachouts_controller.rb
+++ reachout_backend/app/controllers/api/reachouts_controller.rb
@@ -1,2 +1,36 @@
 class Api::ReachoutsController < ApplicationController
+  def index
+    @reachouts = Reachout.all
+    render "index.json.jb"
+  end
+
+  def create
+    @reachout = Reachout.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      phone_number: params[:phone_number],
+    )
+    @reachout.save
+    render "show.json.jb"
+  end
+
+  def show
+    @reachout = Reachout.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @reachout = Reachout.find_by(id: params[:id])
+    @reachout.name = params[:name] || @reachout.name
+    @reachout.width = params[:width] || @reachout.width
+    @reachout.height = params[:height] || @reachout.height
+    @reachout.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    reachout = Reachout.find_by(id: params[:id])
+    reachout.destroy
+    render json: { message: "Reachout successfully destroyed." }
+  end
 end
