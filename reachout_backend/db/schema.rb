419404937ee091dced980ced605243090cbcc01b
taylorod <taylorsunwantedemails@gmail.com>
2020-10-24 13:40:57 -0500

    add jwt gem so login actually works

--- reachout_backend/db/schema.rb
+++ reachout_backend/db/schema.rb
@@ -43,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_10_02_011652) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
