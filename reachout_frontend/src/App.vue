7c5d3598b5c39f089f1ec3719363beeacf166306
taylorod <taylorsunwantedemails@gmail.com>
2020-10-29 19:11:27 -0500

    add login, sign up, and logout button

--- reachout_frontend/src/App.vue
+++ reachout_frontend/src/App.vue
@@ -2,7 +2,11 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/test">Test</router-link> |
+      <router-link to="/signup">Signup</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/logout">Logout</router-link>
     </div>
     <router-view/>
   </div>
