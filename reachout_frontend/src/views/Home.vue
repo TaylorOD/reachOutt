3bdefd9c2d0bb0762e1b2860982d7bcc0e85ccad
taylorod <taylorsunwantedemails@gmail.com>
2020-10-29 20:39:14 -0500

    add index page for reachouts

--- reachout_frontend/src/views/Home.vue
+++ reachout_frontend/src/views/Home.vue
@@ -1,20 +1,39 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h2>All Reachouts</h2>
+    <div v-for="reachout in reachouts">
+      <p>Start Date: {{ reachout.start_date }}</p>
+      <!-- <img v-bind:src="reachout.url" v-bind:alt="reachout.name" /> -->
+      <p>Last Reachout Sent: {{ reachout.last_reachout_sent }}</p>
+      <p>Frequency: {{ reachout.frequency }}</p>
+      <p>Contact ID: {{ reachout.contact_id }}</p>
+      <p>Datatime: {{ reachout.datetime }}</p>
+      -
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+</style>
 
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
-    }
+      reachouts: [],
+    };
   },
-  created: function() {},
-  methods: {},
-}
+  created: function() {
+    this.indexReachouts();
+  },
+  methods: {
+    indexReachouts: function() {
+      axios.get("/api/reachouts").then(response => {
+        console.log("reachouts index", response);
+        this.reachouts = response.data;
+      });
+    },
+  },
+};
 </script>
-
