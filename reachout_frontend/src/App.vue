191d1d1a5ae4acba7592e25ebcdf7cf49661be03
taylorod <taylorsunwantedemails@gmail.com>
2020-11-05 19:29:18 -0600

    add home page

--- reachout_frontend/src/App.vue
+++ reachout_frontend/src/App.vue
@@ -18,11 +18,11 @@
             <div class="collapse navbar-collapse" id="navbar-collapse">
               <ul class="nav navbar-nav ">
                 <li><a href="./" title="">01 : Home</a></li>
-                <li><a href="./contact.html" title="">02 : Contacts</a></li>
-                <li><a href="./reachout.html" title="">03 : Reachouts</a></li>
-                <li><a href="./login.html" title="">04 : Login</a></li>
-                <li><a href="./logout.html" title="">05 : Logout</a></li>
-                <li><a href="./signup.html" title="">06 : Signup</a></li>
+                <li><a href="./contact" title="">02 : Contacts</a></li>
+                <li><a href="./reachout" title="">03 : Reachouts</a></li>
+                <li><a href="./login" title="">04 : Login</a></li>
+                <li><a href="./logout" title="">05 : Logout</a></li>
+                <li><a href="./signup" title="">06 : Signup</a></li>
               </ul>
 
             </div> 
@@ -35,7 +35,51 @@
       <router-link to="/login">Login</router-link> |
       <router-link to="/logout">Logout</router-link> -->
     </div>
+     <div class="hero-full-container background-image-container white-text-container" style="background-image: url('./assets/images/space.jpg')">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="hero-full-wrapper">
+              <div class="text-content">
+                <h1>Hello,<br>
+                  <span id="typed-strings">
+                    <span>I'm a family member</span>
+                    <span>Friend</span>
+                    <span>Someone who misses you</span>
+                    <span>Reachout</span>
+                  </span>
+                  <span id="typed"></span>
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <router-view/>
+
+    <!-- <div class="hero-full-container background-image-container white-text-container" style="background-image: url('./assets/images/space.jpg')">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="hero-full-wrapper">
+              <div class="text-content">
+                <h1>Hello,<br>
+                  <span id="typed-strings">
+                    <span>I'm a family member</span>
+                    <span>Friend</span>
+                    <span>Someone who misses you</span>
+                    <span>Reachout</span>
+                  </span>
+                  <span id="typed"></span>
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+
 
   </div>
 </template>
