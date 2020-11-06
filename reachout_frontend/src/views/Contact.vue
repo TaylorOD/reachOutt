224182dafe7ae2edcae97cafc3438bb2e8601dcb
taylorod <taylorsunwantedemails@gmail.com>
2020-11-05 19:13:28 -0600

    install theme

---
+++ reachout_frontend/src/views/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="contacts-index">
+    <h1>All Contacts</h1>
+    <div v-for="contact in contacts">
+      <p>Phone Number: {{ contact.phone_number }}</p>
+      <p>First Name: {{ contact.first_name }}</p>
+      <p>Last Name: {{ contact.last_name }}</p>
+      -
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      contacts: [],
+    };
+  },
+  created: function() {
+    axios.get("/api/contacts").then(response => {
+      console.log("contacts index", response);
+      this.contacts = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
