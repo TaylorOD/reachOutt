64720bad891025d2be6d90a212d2deb42eb777b6
taylorod <taylorsunwantedemails@gmail.com>
2020-11-08 10:25:02 -0600

    generate new twilio controllerand make message call

---
+++ reachout_backend/app/controllers/notifications_controller.rb
@@ -0,0 +1,15 @@
+class NotificationsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def notify
+    client = Twilio::REST::Client.new Rails.application.credentials.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+
+    message = client.messages.create 
+    from: '+12014236603', 
+    to: '+18025228145', 
+    body: 'Learning to send SMS you are.'
+
+    render plain: message.status
+  end
+
+end
