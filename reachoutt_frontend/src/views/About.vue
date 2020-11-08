8dec99cce12ab9bb0a6fb0f5067ab8c2427acede
taylorod <taylorsunwantedemails@gmail.com>
2020-11-08 16:38:31 -0600

    update footer and header

--- reachoutt_frontend/src/views/About.vue
+++ reachoutt_frontend/src/views/About.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="section-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="section-container-spacer text-center">
+            <h1 class="h2">01 : ReachOutt</h1>
+          </div>
+          
+          <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+              <div class="row">
+                <div class="col-xs-12 col-md-6">
+                  <h3>Consectetur adipiscing elit</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luctus at sem quis varius. 
+                  Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras ultrices hendrerit nisl.</p>
+                  <h3>Ut enim ad minim </h3>
+                  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+                  <h3>Maecenas luctus at sem quis varius</h3>
+                  <p>
+                  Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus iaculis magna sagittis elit sagittis, at hendrerit lorem venenatis. Morbi accumsan iaculis blandit. Cras ultrices hendrerit nisl.</p>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                  <img src="/public/assets/images/space.jpg" class="img-responsive" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          
+       </div>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
