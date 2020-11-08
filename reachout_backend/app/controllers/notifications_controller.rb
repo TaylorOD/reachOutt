92a883a4339c937f46e7654ca5466b6124eb4658
taylorod <taylorsunwantedemails@gmail.com>
2020-11-08 11:42:57 -0600

    sending mms with twilio

--- reachout_backend/app/controllers/notifications_controller.rb
+++ reachout_backend/app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
 
   def notify
     client = Twilio::REST::Client.new Rails.application.credentials.twilio_account_sid, Rails.application.credentials.twilio_auth_token
-    message = client.messages.create from: '12014236603', to: '18025228145', body: 'Learning to send SMS you are.'
+    message = client.messages.create from: '12014236603', to: '18025228145', body: 'Learning to send SMS you are.', media_url: 'https://cdn.wikimg.net/strategywiki/images/d/d6/Zelda_ALttP_Link.gif'
     # render plain: message.status
   end
 
