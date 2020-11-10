c53f1bacd7bdf20c8cc6c7c638ccac212982c73a
taylorod <taylorsunwantedemails@gmail.com>
2020-11-10 10:27:40 -0600

    update seeds file with right phone number format and update freqeucny drop down

--- reachoutt_backend/db/seeds.rb
+++ reachoutt_backend/db/seeds.rb
@@ -15,350 +15,350 @@ User.create!([
     first_name: "Roch",
     last_name: "Geroldo",
     email: "rgeroldo0@smh.com.au",
-    phone_number: "738 390 9486",
+    phone_number: "738-390-9486",
     password: "A63drBh61q1t"
     },
     {
     first_name: "Keri",
     last_name: "Bodesson",
     email: "kbodesson1@dmoz.org",
-    phone_number: "602 190 7194",
+    phone_number: "602-190-7194",
     password: "ITa8ahlQZ77g"
     },
     {
     first_name: "Dominica",
     last_name: "Dallon",
     email: "ddallon2@paypal.com",
-    phone_number: "933 401 6124",
+    phone_number: "933-401-6124",
     password: "ukQuZ1TN"
     },
     {
     first_name: "Clarance",
     last_name: "Botwood",
     email: "cbotwood3@wisc.edu",
-    phone_number: "718 657 9322",
+    phone_number: "718-657-9322",
     password: "PRWIlgrg2Nz"
     },
     {
     first_name: "Inglis",
     last_name: "Grief",
     email: "igrief4@geocities.jp",
-    phone_number: "609 975 8836",
+    phone_number: "609-975-8836",
     password: "y2e9gJxzUm"
     },
     {
     first_name: "Kaine",
     last_name: "Ollander",
     email: "kollander5@ox.ac.uk",
-    phone_number: "296 424 1425",
+    phone_number: "296-424-1425",
     password: "s6CzmFacQ"
     },
     {
     first_name: "Shaylynn",
     last_name: "MacCallister",
     email: "smaccallister6@bandcamp.com",
-    phone_number: "858 906 8817",
+    phone_number: "858-906-8817",
     password: "D4Fo8LVDON7"
     },
     {
     first_name: "Allx",
     last_name: "Inchley",
     email: "ainchley7@nps.gov",
-    phone_number: "469 450 4112",
+    phone_number: "469-450-4112",
     password: "YIvWEEmS33"
     },
     {
     first_name: "Jedediah",
     last_name: "Burrus",
     email: "jburrus8@fastcompany.com",
-    phone_number: "373 842 1143",
+    phone_number: "373-842-1143",
     password: "gcu2jvy"
     },
     {
     first_name: "Clary",
     last_name: "Trowill",
     email: "ctrowill9@imdb.com",
-    phone_number: "485 558 3019",
+    phone_number: "485-558-3019",
     password: "cLrQweTuCC5"
     },
     {
     first_name: "Alair",
     last_name: "Doodney",
     email: "adoodneya@weibo.com",
-    phone_number: "350 788 5809",
+    phone_number: "350-788-5809",
     password: "0Lj9R7lj"
     },
     {
     first_name: "Julina",
     last_name: "Gilliard",
     email: "jgilliardb@paypal.com",
-    phone_number: "944 834 6245",
+    phone_number: "944-834-6245",
     password: "r5IaISM4x"
     },
     {
     first_name: "Adelina",
     last_name: "Dunrige",
     email: "adunrigec@cpanel.net",
-    phone_number: "968 768 7625",
+    phone_number: "968-768-7625",
     password: "Yc73PgpSK"
     },
     {
     first_name: "Jamie",
     last_name: "Brotherheed",
     email: "jbrotherheedd@wikia.com",
-    phone_number: "623 371 0255",
+    phone_number: "623-371-0255",
     password: "QYwkcQ2DyEO"
     },
     {
     first_name: "Ola",
     last_name: "Ingrem",
     email: "oingreme@so-net.ne.jp",
-    phone_number: "241 769 6221",
+    phone_number: "241-769-6221",
     password: "BQT3IFMKs"
     },
     {
     first_name: "Patrice",
     last_name: "Foord",
     email: "pfoordf@google.ru",
-    phone_number: "395 758 3928",
+    phone_number: "395-758-3928",
     password: "BzFQPsK"
     },
     {
     first_name: "Fanchette",
     last_name: "Crinage",
     email: "fcrinageg@wunderground.com",
-    phone_number: "745 749 9421",
+    phone_number: "745-749-9421",
     password: "sbydHbz"
     },
     {
     first_name: "Nestor",
     last_name: "Reilly",
     email: "nreillyh@vk.com",
-    phone_number: "885 401 0792",
+    phone_number: "885-401-0792",
     password: "yp1VGLqj"
     },
     {
     first_name: "Celestyn",
     last_name: "Warriner",
     email: "cwarrineri@wired.com",
-    phone_number: "935 943 1149",
+    phone_number: "935-943-1149",
     password: "rBU63W"
     },
     {
     first_name: "Izaak",
     last_name: "Jeffree",
     email: "ijeffreej@yahoo.co.jp",
-    phone_number: "923 840 3371",
+    phone_number: "923-840-3371",
     password: "WLS8jhZIN"
     },
     {
     first_name: "Arch",
     last_name: "Woodyeare",
     email: "awoodyearek@uol.com.br",
-    phone_number: "367 912 2575",
+    phone_number: "367-912-2575",
     password: "lBxHJWDDM"
     },
     {
     first_name: "Karlik",
     last_name: "Lelande",
     email: "klelandel@elegantthemes.com",
-    phone_number: "154 919 9685",
+    phone_number: "154-919-9685",
     password: "TB6urC"
     },
     {
     first_name: "Vivianne",
     last_name: "Sherar",
     email: "vsherarm@live.com",
-    phone_number: "650 693 3274",
+    phone_number: "650-693-3274",
     password: "yCUD7Oo7Fy"
     },
     {
     first_name: "Jeanne",
     last_name: "Bysaker",
     email: "jbysakern@sfgate.com",
-    phone_number: "736 671 0758",
+    phone_number: "736-671-0758",
     password: "yqkE91WzZ"
     },
     {
     first_name: "Tiena",
     last_name: "Ashdown",
     email: "tashdowno@istockphoto.com",
-    phone_number: "531 440 2621",
+    phone_number: "531-440-2621",
     password: "Ercp7Z3f"
     },
     {
     first_name: "Madelena",
     last_name: "Whatling",
     email: "mwhatlingp@globo.com",
-    phone_number: "763 534 2320",
+    phone_number: "763-534-2320",
     password: "XN8zIKzo"
     },
     {
     first_name: "Jobina",
     last_name: "Theurer",
     email: "jtheurerq@t.co",
-    phone_number: "931 733 4889",
+    phone_number: "931-733-4889",
     password: "H7lZXdg7K"
     },
     {
     first_name: "Helge",
     last_name: "Arstingall",
     email: "harstingallr@usda.gov",
-    phone_number: "620 908 8702",
+    phone_number: "620-908-8702",
     password: "hgx6JdJQ"
     },
     {
     first_name: "Matthieu",
     last_name: "Herche",
     email: "mherches@tuttocitta.it",
-    phone_number: "175 736 9439",
+    phone_number: "175-736-9439",
     password: "VUFneu5bNBQ"
     },
     {
     first_name: "Minta",
     last_name: "Bullocke",
     email: "mbullocket@geocities.jp",
-    phone_number: "345 322 0293",
+    phone_number: "345-322-0293",
     password: "8cbX2XhbmFdh"
     },
     {
     first_name: "Jillane",
     last_name: "Patrono",
     email: "jpatronou@sourceforge.net",
-    phone_number: "231 904 0507",
+    phone_number: "231-904-0507",
     password: "YEKMTN"
     },
     {
     first_name: "Natale",
     last_name: "Cawston",
     email: "ncawstonv@jiathis.com",
-    phone_number: "474 227 6691",
+    phone_number: "474-227-6691",
     password: "xIiADa73m"
     },
     {
     first_name: "Kendell",
     last_name: "Venditto",
     email: "kvendittow@sogou.com",
-    phone_number: "184 514 9180",
+    phone_number: "184-514-9180",
     password: "uVnAnyRC"
     },
     {
     first_name: "Sandie",
     last_name: "Fancutt",
     email: "sfancuttx@epa.gov",
-    phone_number: "298 666 9565",
+    phone_number: "298-666-9565",
     password: "24ezuS8k0"
     },
     {
     first_name: "Dione",
     last_name: "Posten",
     email: "dposteny@bandcamp.com",
-    phone_number: "931 797 2669",
+    phone_number: "931-797-2669",
     password: "JgLmxSrY3"
     },
     {
     first_name: "Lira",
     last_name: "Stebbin",
     email: "lstebbinz@cnet.com",
-    phone_number: "532 558 3945",
+    phone_number: "532-558-3945",
     password: "4E96xwkQi"
     },
     {
     first_name: "Christalle",
     last_name: "MacLaughlin",
     email: "cmaclaughlin10@wired.com",
-    phone_number: "462 637 3881",
+    phone_number: "462-637-3881",
     password: "mqmTf9iCm"
     },
     {
     first_name: "Rania",
     last_name: "Gover",
     email: "rgover11@answers.com",
-    phone_number: "696 493 1924",
+    phone_number: "696-493-1924",
     password: "XK0QZB"
     },
     {
     first_name: "Coop",
     last_name: "Fairall",
     email: "cfairall12@yahoo.com",
-    phone_number: "408 140 1858",
+    phone_number: "408-140-1858",
     password: "EhQbbrJ478"
     },
     {
     first_name: "Teodorico",
     last_name: "Wooster",
     email: "twooster13@goo.gl",
-    phone_number: "969 538 9228",
+    phone_number: "969-538-9228",
     password: "fMY7WrWtVO"
     },
     {
     first_name: "Yvonne",
     last_name: "Gruszka",
     email: "ygruszka14@bloglovin.com",
-    phone_number: "125 582 1150",
+    phone_number: "125-582-1150",
     password: "2bVAVjjTZ5l6"
     },
     {
     first_name: "Ronna",
     last_name: "Oby",
     email: "roby15@dot.gov",
-    phone_number: "212 148 7646",
+    phone_number: "212-148-7646",
     password: "Xba4WnEDh7S"
     },
     {
     first_name: "Roxie",
     last_name: "Illing",
     email: "rilling16@theatlantic.com",
-    phone_number: "902 637 6874",
+    phone_number: "902-637-6874",
     password: "UHzG3bP1zOV"
     },
     {
     first_name: "Dalila",
     last_name: "Peschka",
     email: "dpeschka17@ucoz.com",
-    phone_number: "969 750 5163",
+    phone_number: "969-750-5163",
     password: "KJd75w1d"
     },
     {
     first_name: "Zora",
     last_name: "Colquyte",
     email: "zcolquyte18@phpbb.com",
-    phone_number: "970 812 4779",
+    phone_number: "970-812-4779",
     password: "ItNMAGUTUfEE"
     },
     {
     first_name: "Brendan",
     last_name: "Brikner",
     email: "bbrikner19@last.fm",
-    phone_number: "562 769 3964",
+    phone_number: "562-769-3964",
     password: "9Klj4KU"
     },
     {
     first_name: "Kaitlin",
     last_name: "McCanny",
     email: "kmccanny1a@europa.eu",
-    phone_number: "470 884 1866",
+    phone_number: "470-884-1866",
     password: "lknJnvqlM"
     },
     {
     first_name: "Reinaldo",
     last_name: "Braz",
     email: "rbraz1b@dot.gov",
-    phone_number: "399 836 9800",
+    phone_number: "399-836-9800",
     password: "RIfH3j"
     },
     {
     first_name: "Bradford",
     last_name: "McNamara",
     email: "bmcnamara1c@ft.com",
-    phone_number: "319 535 0580",
+    phone_number: "319-535-0580",
     password: "gozSFSr"
     },
     {
     first_name: "Aubrey",
     last_name: "Wallen",
     email: "awallen1d@devhub.com",
-    phone_number: "445 153 2974",
+    phone_number: "445-153-2974",
     password: "QoCblmFN"
     },
 ])
