62b576fa1d0cf3bf4590667978993222cebab7b5
taylorod <taylorsunwantedemails@gmail.com>
2020-11-10 08:55:04 -0600

    add new reachout start message and update phone number to be users

--- reachoutt_backend/app/controllers/api/reachouts_controller.rb
+++ reachoutt_backend/app/controllers/api/reachouts_controller.rb
@@ -19,12 +19,13 @@ class Api::ReachoutsController < ApplicationController
     if @reachout.save
       render "show.json.jb"
       client = Twilio::REST::Client.new Rails.application.credentials.twilio_account_sid, Rails.application.credentials.twilio_auth_token
-      message = client.messages.create from: '12014236603', to: '18025228145', body: "New Reachout Created for #{@reachout.contact.first_name} #{@reachout.contact.last_name}"
+      message = client.messages.create from: '12014236603', to: "#{@reachout.user.phone_number}", body: "Congrats! You created a new ReachOutt for #{@reachout.contact.first_name} #{@reachout.contact.last_name} starting on #{@reachout.start_date}. 
+      ReachOutt will remind you to reachout to #{@reachout.contact.first_name} every #{@reachout.frequency}."
       s = Rufus::Scheduler.singleton
       s.every @reachout.frequency do
         puts "Reachout to #{@reachout.contact_id}"
         client = Twilio::REST::Client.new Rails.application.credentials.twilio_account_sid, Rails.application.credentials.twilio_auth_token
-        message = client.messages.create from: '12014236603', to: '18025228145', body: "Hey! It's time to reachout to #{@reachout.contact.first_name} #{@reachout.contact.last_name} at #{@reachout.contact.phone_number}"  
+        message = client.messages.create from: '12014236603', to: "#{@reachout.user.phone_number}", body: "Hey! It's been #{@reachout.frequency}. Time to reachout to #{@reachout.contact.first_name} #{@reachout.contact.last_name} at #{@reachout.contact.phone_number}"  
       end
     else
       render json: { errors: @reachout.errors.full_messages }, status: :bad_request
