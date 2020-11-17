1a9517637353f21e9e269787abbc3c56d7fc89d8
taylorod <taylorsunwantedemails@gmail.com>
2020-11-16 18:35:39 -0600

    add updated seeds file

--- reachoutt_backend/db/seeds.rb
+++ reachoutt_backend/db/seeds.rb
@@ -9,415 +9,68 @@
 User.create!([
   { email: "jonasdorsett@gmail.com", password: "password", first_name: "Jonas", last_name: "Dorsett", phone_number: "802-522-7788" },
   { email: "dorsetttaylor@gmail.com", password: "password", first_name: "Taylor", last_name: "Dorsett", phone_number: "802-522-8145" },
-  { email: "luke@example.com", password: "password", first_name: "Luke", last_name: "Booker", phone_number: "802-557-0433" },
   { email: "leahshawd@gmail.com", password: "password", first_name: "Leah", last_name: "Shaw", phone_number: "815-878-3186" },
-  {
-    first_name: "Roch",
-    last_name: "Geroldo",
-    email: "rgeroldo0@smh.com.au",
-    phone_number: "738-390-9486",
-    password: "A63drBh61q1t"
-    },
-    {
-    first_name: "Keri",
-    last_name: "Bodesson",
-    email: "kbodesson1@dmoz.org",
-    phone_number: "602-190-7194",
-    password: "ITa8ahlQZ77g"
-    },
-    {
-    first_name: "Dominica",
-    last_name: "Dallon",
-    email: "ddallon2@paypal.com",
-    phone_number: "933-401-6124",
-    password: "ukQuZ1TN"
-    },
-    {
-    first_name: "Clarance",
-    last_name: "Botwood",
-    email: "cbotwood3@wisc.edu",
-    phone_number: "718-657-9322",
-    password: "PRWIlgrg2Nz"
-    },
-    {
-    first_name: "Inglis",
-    last_name: "Grief",
-    email: "igrief4@geocities.jp",
-    phone_number: "609-975-8836",
-    password: "y2e9gJxzUm"
-    },
-    {
-    first_name: "Kaine",
-    last_name: "Ollander",
-    email: "kollander5@ox.ac.uk",
-    phone_number: "296-424-1425",
-    password: "s6CzmFacQ"
-    },
-    {
-    first_name: "Shaylynn",
-    last_name: "MacCallister",
-    email: "smaccallister6@bandcamp.com",
-    phone_number: "858-906-8817",
-    password: "D4Fo8LVDON7"
-    },
-    {
-    first_name: "Allx",
-    last_name: "Inchley",
-    email: "ainchley7@nps.gov",
-    phone_number: "469-450-4112",
-    password: "YIvWEEmS33"
-    },
-    {
-    first_name: "Jedediah",
-    last_name: "Burrus",
-    email: "jburrus8@fastcompany.com",
-    phone_number: "373-842-1143",
-    password: "gcu2jvy"
-    },
-    {
-    first_name: "Clary",
-    last_name: "Trowill",
-    email: "ctrowill9@imdb.com",
-    phone_number: "485-558-3019",
-    password: "cLrQweTuCC5"
-    },
-    {
-    first_name: "Alair",
-    last_name: "Doodney",
-    email: "adoodneya@weibo.com",
-    phone_number: "350-788-5809",
-    password: "0Lj9R7lj"
-    },
-    {
-    first_name: "Julina",
-    last_name: "Gilliard",
-    email: "jgilliardb@paypal.com",
-    phone_number: "944-834-6245",
-    password: "r5IaISM4x"
-    },
-    {
-    first_name: "Adelina",
-    last_name: "Dunrige",
-    email: "adunrigec@cpanel.net",
-    phone_number: "968-768-7625",
-    password: "Yc73PgpSK"
-    },
-    {
-    first_name: "Jamie",
-    last_name: "Brotherheed",
-    email: "jbrotherheedd@wikia.com",
-    phone_number: "623-371-0255",
-    password: "QYwkcQ2DyEO"
-    },
-    {
-    first_name: "Ola",
-    last_name: "Ingrem",
-    email: "oingreme@so-net.ne.jp",
-    phone_number: "241-769-6221",
-    password: "BQT3IFMKs"
-    },
-    {
-    first_name: "Patrice",
-    last_name: "Foord",
-    email: "pfoordf@google.ru",
-    phone_number: "395-758-3928",
-    password: "BzFQPsK"
-    },
-    {
-    first_name: "Fanchette",
-    last_name: "Crinage",
-    email: "fcrinageg@wunderground.com",
-    phone_number: "745-749-9421",
-    password: "sbydHbz"
-    },
-    {
-    first_name: "Nestor",
-    last_name: "Reilly",
-    email: "nreillyh@vk.com",
-    phone_number: "885-401-0792",
-    password: "yp1VGLqj"
-    },
-    {
-    first_name: "Celestyn",
-    last_name: "Warriner",
-    email: "cwarrineri@wired.com",
-    phone_number: "935-943-1149",
-    password: "rBU63W"
-    },
-    {
-    first_name: "Izaak",
-    last_name: "Jeffree",
-    email: "ijeffreej@yahoo.co.jp",
-    phone_number: "923-840-3371",
-    password: "WLS8jhZIN"
-    },
-    {
-    first_name: "Arch",
-    last_name: "Woodyeare",
-    email: "awoodyearek@uol.com.br",
-    phone_number: "367-912-2575",
-    password: "lBxHJWDDM"
-    },
-    {
-    first_name: "Karlik",
-    last_name: "Lelande",
-    email: "klelandel@elegantthemes.com",
-    phone_number: "154-919-9685",
-    password: "TB6urC"
-    },
-    {
-    first_name: "Vivianne",
-    last_name: "Sherar",
-    email: "vsherarm@live.com",
-    phone_number: "650-693-3274",
-    password: "yCUD7Oo7Fy"
-    },
-    {
-    first_name: "Jeanne",
-    last_name: "Bysaker",
-    email: "jbysakern@sfgate.com",
-    phone_number: "736-671-0758",
-    password: "yqkE91WzZ"
-    },
-    {
-    first_name: "Tiena",
-    last_name: "Ashdown",
-    email: "tashdowno@istockphoto.com",
-    phone_number: "531-440-2621",
-    password: "Ercp7Z3f"
-    },
-    {
-    first_name: "Madelena",
-    last_name: "Whatling",
-    email: "mwhatlingp@globo.com",
-    phone_number: "763-534-2320",
-    password: "XN8zIKzo"
-    },
-    {
-    first_name: "Jobina",
-    last_name: "Theurer",
-    email: "jtheurerq@t.co",
-    phone_number: "931-733-4889",
-    password: "H7lZXdg7K"
-    },
-    {
-    first_name: "Helge",
-    last_name: "Arstingall",
-    email: "harstingallr@usda.gov",
-    phone_number: "620-908-8702",
-    password: "hgx6JdJQ"
-    },
-    {
-    first_name: "Matthieu",
-    last_name: "Herche",
-    email: "mherches@tuttocitta.it",
-    phone_number: "175-736-9439",
-    password: "VUFneu5bNBQ"
-    },
-    {
-    first_name: "Minta",
-    last_name: "Bullocke",
-    email: "mbullocket@geocities.jp",
-    phone_number: "345-322-0293",
-    password: "8cbX2XhbmFdh"
-    },
-    {
-    first_name: "Jillane",
-    last_name: "Patrono",
-    email: "jpatronou@sourceforge.net",
-    phone_number: "231-904-0507",
-    password: "YEKMTN"
-    },
-    {
-    first_name: "Natale",
-    last_name: "Cawston",
-    email: "ncawstonv@jiathis.com",
-    phone_number: "474-227-6691",
-    password: "xIiADa73m"
-    },
-    {
-    first_name: "Kendell",
-    last_name: "Venditto",
-    email: "kvendittow@sogou.com",
-    phone_number: "184-514-9180",
-    password: "uVnAnyRC"
-    },
-    {
-    first_name: "Sandie",
-    last_name: "Fancutt",
-    email: "sfancuttx@epa.gov",
-    phone_number: "298-666-9565",
-    password: "24ezuS8k0"
-    },
-    {
-    first_name: "Dione",
-    last_name: "Posten",
-    email: "dposteny@bandcamp.com",
-    phone_number: "931-797-2669",
-    password: "JgLmxSrY3"
-    },
-    {
-    first_name: "Lira",
-    last_name: "Stebbin",
-    email: "lstebbinz@cnet.com",
-    phone_number: "532-558-3945",
-    password: "4E96xwkQi"
-    },
-    {
-    first_name: "Christalle",
-    last_name: "MacLaughlin",
-    email: "cmaclaughlin10@wired.com",
-    phone_number: "462-637-3881",
-    password: "mqmTf9iCm"
-    },
-    {
-    first_name: "Rania",
-    last_name: "Gover",
-    email: "rgover11@answers.com",
-    phone_number: "696-493-1924",
-    password: "XK0QZB"
-    },
-    {
-    first_name: "Coop",
-    last_name: "Fairall",
-    email: "cfairall12@yahoo.com",
-    phone_number: "408-140-1858",
-    password: "EhQbbrJ478"
-    },
-    {
-    first_name: "Teodorico",
-    last_name: "Wooster",
-    email: "twooster13@goo.gl",
-    phone_number: "969-538-9228",
-    password: "fMY7WrWtVO"
-    },
-    {
-    first_name: "Yvonne",
-    last_name: "Gruszka",
-    email: "ygruszka14@bloglovin.com",
-    phone_number: "125-582-1150",
-    password: "2bVAVjjTZ5l6"
-    },
-    {
-    first_name: "Ronna",
-    last_name: "Oby",
-    email: "roby15@dot.gov",
-    phone_number: "212-148-7646",
-    password: "Xba4WnEDh7S"
-    },
-    {
-    first_name: "Roxie",
-    last_name: "Illing",
-    email: "rilling16@theatlantic.com",
-    phone_number: "902-637-6874",
-    password: "UHzG3bP1zOV"
-    },
-    {
-    first_name: "Dalila",
-    last_name: "Peschka",
-    email: "dpeschka17@ucoz.com",
-    phone_number: "969-750-5163",
-    password: "KJd75w1d"
-    },
-    {
-    first_name: "Zora",
-    last_name: "Colquyte",
-    email: "zcolquyte18@phpbb.com",
-    phone_number: "970-812-4779",
-    password: "ItNMAGUTUfEE"
-    },
-    {
-    first_name: "Brendan",
-    last_name: "Brikner",
-    email: "bbrikner19@last.fm",
-    phone_number: "562-769-3964",
-    password: "9Klj4KU"
-    },
-    {
-    first_name: "Kaitlin",
-    last_name: "McCanny",
-    email: "kmccanny1a@europa.eu",
-    phone_number: "470-884-1866",
-    password: "lknJnvqlM"
-    },
-    {
-    first_name: "Reinaldo",
-    last_name: "Braz",
-    email: "rbraz1b@dot.gov",
-    phone_number: "399-836-9800",
-    password: "RIfH3j"
-    },
-    {
-    first_name: "Bradford",
-    last_name: "McNamara",
-    email: "bmcnamara1c@ft.com",
-    phone_number: "319-535-0580",
-    password: "gozSFSr"
-    },
-    {
-    first_name: "Aubrey",
-    last_name: "Wallen",
-    email: "awallen1d@devhub.com",
-    phone_number: "445-153-2974",
-    password: "QoCblmFN"
-    },
+  { email: "lbooker@test.com", password: "password", first_name: "Luke", last_name: "Booker", phone_number: "802-557-0433" },
+  { first_name: "Roch", last_name: "Geroldo", email: "rgeroldo0@test.com", phone_number: "738-390-9486", password: "A63drBh61q1t" },
+  { first_name: "Keri", last_name: "Bodesson", email: "kbodesson1@dmoz.org", phone_number: "602-190-7194", password: "ITa8ahlQZ77g" },
+  { first_name: "Dominica", last_name: "Dallon", email: "ddallon2@paypal.com", phone_number: "933-401-6124", password: "ukQuZ1TN" }
 ])
 
 Contact.create!([
   { phone_number: "802-522-7788", first_name: "Jonas", last_name: "Dorsett", user_id: 1 },
-  { phone_number: "802-557-0433", first_name: "Luke", last_name: "Rockhold", user_id: 2 },
-  { phone_number: "802-522-8145", first_name: "Taylor", last_name: "Dorsett", user_id: 2 },
+  { phone_number: "802-557-0433", first_name: "Luke", last_name: "Rockhold", user_id: 1 },
+  { phone_number: "802-522-8145", first_name: "Taylor", last_name: "Dorsett", user_id: 1 },
   { phone_number: "815-878-3186", first_name: "Leah", last_name: "Shaw", user_id: 1 },
-  { phone_number: "317-683-3160", first_name: "Katherine", last_name: "Tourmell", user_id: 17 },
-  { phone_number: "276-476-2123", first_name: "Valida", last_name: "Ciccottini", user_id: 46 },
-  { phone_number: "547-212-7796", first_name: "Natala", last_name: "Brownhill", user_id: 49 },
-  { phone_number: "458-683-7943", first_name: "Giselbert", last_name: "Newbery", user_id: 12 },
-  { phone_number: "660-545-0014", first_name: "Mabel", last_name: "Amiss", user_id: 39 },
-  { phone_number: "315-977-4138", first_name: "Ermengarde", last_name: "Raynham", user_id: 36 },
-  { phone_number: "902-739-9860", first_name: "Burt", last_name: "Kastel", user_id: 27 },
-  { phone_number: "276-560-2432", first_name: "Ellyn", last_name: "Lacheze", user_id: 17 },
-  { phone_number: "826-990-4326", first_name: "Tonie", last_name: "Roarty", user_id: 3 },
-  { phone_number: "886-784-0361", first_name: "Chad", last_name: "MacAlester", user_id: 49 },
-  { phone_number: "772-755-6845", first_name: "Gardy", last_name: "Kach", user_id: 3 },
-  { phone_number: "247-950-4432", first_name: "Calv", last_name: "Aspole", user_id: 32 },
-  { phone_number: "705-116-1199", first_name: "Berty", last_name: "Ardley", user_id: 28 },
-  { phone_number: "634-702-2699", first_name: "Tobey", last_name: "Shedden", user_id: 14 },
-  { phone_number: "179-367-4782", first_name: "Ambrosio", last_name: "Klarzynski", user_id: 17 },
-  { phone_number: "873-877-0209", first_name: "Waverley", last_name: "Brantl", user_id: 43 },
-  { phone_number: "178-865-1202", first_name: "Lukas", last_name: "Robbey", user_id: 30 },
-  { phone_number: "676-616-7956", first_name: "Chariot", last_name: "Fenty", user_id: 9 },
-  { phone_number: "432-654-7820", first_name: "Corissa", last_name: "Abramamov", user_id: 9 },
-  { phone_number: "681-403-8502", first_name: "Frasquito", last_name: "Pedrielli", user_id: 18 },
-  { phone_number: "227-843-1342", first_name: "Cora", last_name: "Jeanes", user_id: 16 },
-  { phone_number: "993-306-6072", first_name: "Garnette", last_name: "Bailes", user_id: 40 },
-  { phone_number: "794-639-2463", first_name: "Henrik", last_name: "Barnet", user_id: 21 },
-  { phone_number: "242-665-4129", first_name: "Fawnia", last_name: "Dominighi", user_id: 50 },
-  { phone_number: "826-706-9037", first_name: "Barrie", last_name: "Philpotts", user_id: 39 },
-  { phone_number: "592-636-5198", first_name: "Myrlene", last_name: "Melton", user_id: 13 },
-  { phone_number: "952-564-5799", first_name: "Ivie", last_name: "Jerosch", user_id: 29 },
-  { phone_number: "840-655-8070", first_name: "Belinda", last_name: "Farrance", user_id: 14 },
-  { phone_number: "597-866-8799", first_name: "Quill", last_name: "Skones", user_id: 33 },
-  { phone_number: "690-642-4526", first_name: "Courtnay", last_name: "Gogarty", user_id: 25 },
-  { phone_number: "102-289-0392", first_name: "Luigi", last_name: "Herkess", user_id: 41 },
-  { phone_number: "562-380-5626", first_name: "Agustin", last_name: "Tinsley", user_id: 17 },
-  { phone_number: "980-819-0827", first_name: "Marianna", last_name: "Itzkovich", user_id: 17 },
-  { phone_number: "200-141-2465", first_name: "Ted", last_name: "Opie", user_id: 36 },
-  { phone_number: "530-545-3834", first_name: "Khalil", last_name: "Bend", user_id: 29 },
-  { phone_number: "602-438-6517", first_name: "Christalle", last_name: "Strase", user_id: 5 },
-  { phone_number: "357-883-8302", first_name: "Belia", last_name: "Ardling", user_id: 16 },
-  { phone_number: "772-705-8867", first_name: "Margarethe", last_name: "Benitez", user_id: 32 },
-  { phone_number: "223-199-4235", first_name: "Gabriel", last_name: "Upstone", user_id: 32 },
-  { phone_number: "272-654-8874", first_name: "Noell", last_name: "Meanwell", user_id: 12 },
-  { phone_number: "920-864-9725", first_name: "Matthew", last_name: "Adderley", user_id: 43 },
-  { phone_number: "447-763-3332", first_name: "Selene", last_name: "Clemence", user_id: 36 },
-  { phone_number: "483-637-1616", first_name: "Rolando", last_name: "Board", user_id: 16 },
-  { phone_number: "336-600-2428", first_name: "Atlanta", last_name: "MacCoveney", user_id: 36 },
-  { phone_number: "386-155-1502", first_name: "Ryon", last_name: "McKinney", user_id: 36 },
-  { phone_number: "451-268-6748", first_name: "Issy", last_name: "Gawn", user_id: 30 },
-  { phone_number: "801-882-5721", first_name: "Trisha", last_name: "Jerams", user_id: 4 },
-  { phone_number: "893-953-3992", first_name: "Adara", last_name: "Darlow", user_id: 44 },
-  { phone_number: "772-315-7020", first_name: "Karleen", last_name: "Crompton", user_id: 45 },
-  { phone_number: "381-937-1031", first_name: "Phillie", last_name: "Hendrich", user_id: 11 }
+  { phone_number: "317-683-3160", first_name: "Katherine", last_name: "Tourmell", user_id: 1 },
+  { phone_number: "276-476-2123", first_name: "Valida", last_name: "Ciccottini", user_id: 1 },
+  { phone_number: "547-212-7796", first_name: "Natala", last_name: "Brownhill", user_id: 1 },
+  { phone_number: "458-683-7943", first_name: "Giselbert", last_name: "Newbery", user_id: 1 },
+  { phone_number: "660-545-0014", first_name: "Mabel", last_name: "Amiss", user_id: 1 },
+  { phone_number: "315-977-4138", first_name: "Ermengarde", last_name: "Raynham", user_id: 1 },
+  { phone_number: "902-739-9860", first_name: "Burt", last_name: "Kastel", user_id: 1 },
+  { phone_number: "276-560-2432", first_name: "Ellyn", last_name: "Lacheze", user_id: 1 },
+  { phone_number: "826-990-4326", first_name: "Tonie", last_name: "Roarty", user_id: 1 },
+  { phone_number: "886-784-0361", first_name: "Chad", last_name: "MacAlester", user_id: 1 },
+  { phone_number: "772-755-6845", first_name: "Gardy", last_name: "Kach", user_id: 1 },
+  { phone_number: "247-950-4432", first_name: "Calv", last_name: "Aspole", user_id: 1 },
+  { phone_number: "705-116-1199", first_name: "Berty", last_name: "Ardley", user_id: 1 },
+  { phone_number: "634-702-2699", first_name: "Tobey", last_name: "Shedden", user_id: 1 },
+  { phone_number: "179-367-4782", first_name: "Ambrosio", last_name: "Klarzynski", user_id: 1 },
+  { phone_number: "873-877-0209", first_name: "Waverley", last_name: "Brantl", user_id: 1 },
+  { phone_number: "178-865-1202", first_name: "Lukas", last_name: "Robbey", user_id: 2 },
+  { phone_number: "676-616-7956", first_name: "Chariot", last_name: "Fenty", user_id: 2 },
+  { phone_number: "432-654-7820", first_name: "Corissa", last_name: "Abramamov", user_id: 2 },
+  { phone_number: "681-403-8502", first_name: "Frasquito", last_name: "Pedrielli", user_id: 2 },
+  { phone_number: "227-843-1342", first_name: "Cora", last_name: "Jeanes", user_id: 2 },
+  { phone_number: "993-306-6072", first_name: "Garnette", last_name: "Bailes", user_id: 2 },
+  { phone_number: "794-639-2463", first_name: "Henrik", last_name: "Barnet", user_id: 3 },
+  { phone_number: "242-665-4129", first_name: "Fawnia", last_name: "Dominighi", user_id: 3 },
+  { phone_number: "826-706-9037", first_name: "Barrie", last_name: "Philpotts", user_id: 3 },
+  { phone_number: "592-636-5198", first_name: "Myrlene", last_name: "Melton", user_id: 3 },
+  { phone_number: "952-564-5799", first_name: "Ivie", last_name: "Jerosch", user_id: 3 },
+  { phone_number: "840-655-8070", first_name: "Belinda", last_name: "Farrance", user_id: 3 },
+  { phone_number: "597-866-8799", first_name: "Quill", last_name: "Skones", user_id: 3 },
+  { phone_number: "690-642-4526", first_name: "Courtnay", last_name: "Gogarty", user_id: 3 },
+  { phone_number: "102-289-0392", first_name: "Luigi", last_name: "Herkess", user_id: 3 },
+  { phone_number: "562-380-5626", first_name: "Agustin", last_name: "Tinsley", user_id: 3 },
+  { phone_number: "980-819-0827", first_name: "Marianna", last_name: "Itzkovich", user_id: 3 },
+  { phone_number: "200-141-2465", first_name: "Ted", last_name: "Opie", user_id: 3 },
+  { phone_number: "530-545-3834", first_name: "Khalil", last_name: "Bend", user_id: 3 },
+  { phone_number: "602-438-6517", first_name: "Christalle", last_name: "Strase", user_id: 3 },
+  { phone_number: "357-883-8302", first_name: "Belia", last_name: "Ardling", user_id: 4 },
+  { phone_number: "772-705-8867", first_name: "Margarethe", last_name: "Benitez", user_id: 5 },
+  { phone_number: "223-199-4235", first_name: "Gabriel", last_name: "Upstone", user_id: 4 },
+  { phone_number: "272-654-8874", first_name: "Noell", last_name: "Meanwell", user_id: 5 },
+  { phone_number: "920-864-9725", first_name: "Matthew", last_name: "Adderley", user_id: 4 },
+  { phone_number: "447-763-3332", first_name: "Selene", last_name: "Clemence", user_id: 5 },
+  { phone_number: "483-637-1616", first_name: "Rolando", last_name: "Board", user_id: 4 },
+  { phone_number: "336-600-2428", first_name: "Atlanta", last_name: "MacCoveney", user_id: 5 },
+  { phone_number: "386-155-1502", first_name: "Ryon", last_name: "McKinney", user_id: 6 },
+  { phone_number: "451-268-6748", first_name: "Issy", last_name: "Gawn", user_id: 6 },
+  { phone_number: "801-882-5721", first_name: "Trisha", last_name: "Jerams", user_id: 6 },
+  { phone_number: "893-953-3992", first_name: "Adara", last_name: "Darlow", user_id: 7 },
+  { phone_number: "772-315-7020", first_name: "Karleen", last_name: "Crompton", user_id: 7 },
+  { phone_number: "381-937-1031", first_name: "Phillie", last_name: "Hendrich", user_id: 7 }
 ])
 
 Reachout.create!([
