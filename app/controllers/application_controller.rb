8d1975142920040dac78a8a7c0bf34f8852d6f21
Taylor Dorsett <taylordorsett@Taylors-MacBook-Air.local>
2023-11-23 19:09:10 -0700

    remove unwanted verify call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token
 
   def current_user
     auth_headers = request.headers["Authorization"]
